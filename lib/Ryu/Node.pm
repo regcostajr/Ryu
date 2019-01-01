fafd15deafe4d03981297c52bae18d971fd83465
Tom Molesworth <TEAM@cpan.org>
2019-01-02 00:20:23 +0800

    Version 0.034

---
+++ lib/Ryu/Node.pm
@@ -0,0 +1,114 @@
+package Ryu::Node;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.034'; # VERSION
+
+=head1 NAME
+
+Ryu::Node - generic node
+
+=head1 DESCRIPTION
+
+This is a common base class for all sources, sinks and other related things.
+It does very little.
+
+=cut
+
+=head1 METHODS
+
+Not really. There's a constructor, but that's not particularly exciting.
+
+=cut
+
+sub new {
+    bless {
+        pause_propagation => 1,
+        @_[1..$#_]
+    }, $_[0]
+}
+
+=head2 pause
+
+Does nothing useful.
+
+=cut
+
+sub pause {
+    use Scalar::Util qw(refaddr);
+    my ($self, $src) = @_;
+    my $k = (refaddr $src) // 0;
+
+    my $was_paused = keys %{$self->{is_paused}};
+    ++$self->{is_paused}{$k};
+    if(my $parent = $self->parent) {
+        $parent->pause($self) if $self->{pause_propagation};
+    }
+    if(my $flow_control = $self->{flow_control}) {
+        $flow_control->emit(0) unless $was_paused;
+    }
+    $self
+}
+
+=head2 resume
+
+Is about as much use as L</pause>.
+
+=cut
+
+sub resume {
+    use Scalar::Util qw(refaddr);
+    my ($self, $src) = @_;
+    my $k = refaddr($src) // 0;
+    delete $self->{is_paused}{$k} unless --$self->{is_paused}{$k} > 0;
+    unless(keys %{$self->{is_paused} || {} }) {
+        if(my $parent = $self->parent) {
+            $parent->resume($self) if $self->{pause_propagation};
+        }
+        if(my $flow_control = $self->{flow_control}) {
+            $flow_control->emit(1);
+        }
+    }
+    $self
+}
+
+=head2 is_paused
+
+Might return 1 or 0, but is generally meaningless.
+
+=cut
+
+sub is_paused {
+    use Scalar::Util qw(refaddr);
+    my ($self, $obj) = @_;
+    return keys %{ $self->{is_paused} } ? 1 : 0 unless defined $obj;
+    my $k = refaddr($obj);
+    return exists $self->{is_paused}{$k}
+    ? 0 + $self->{is_paused}{$k}
+    : 0;
+}
+
+sub flow_control {
+    my ($self) = @_;
+    $self->{flow_control} //= Ryu::Source->new(
+        new_future => $self->{new_future}
+    )
+}
+
+sub label { shift->{label} }
+
+sub parent { shift->{parent} }
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Tom Molesworth <TEAM@cpan.org>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2011-2019. Licensed under the same terms as Perl itself.
+
---
+++ lib/Ryu/Sink.pm
@@ -0,0 +1,92 @@
+package Ryu::Sink;
+
+use strict;
+use warnings;
+
+use parent qw(Ryu::Node);
+
+our $VERSION = '0.034'; # VERSION
+
+=head1 NAME
+
+Ryu::Sink - base representation for a thing that receives events
+
+=head1 DESCRIPTION
+
+This is currently of limited utility.
+
+ my $src = Ryu::Source->new;
+ my $sink = Ryu::Sink->new;
+ $sink->from($src);
+ $sink->source->say;
+
+=cut
+
+use Future;
+
+=head1 METHODS
+
+=cut
+
+sub new {
+    my $class = shift;
+    $class->SUPER::new(
+        is_paused => 0,
+        @_
+    )
+}
+
+=head2 from
+
+Given a source, will attach it as the input for this sink.
+
+=cut
+
+sub from {
+    my ($self, $src, %args) = @_;
+
+    die 'expected a subclass of Ryu::Source, received ' . $src . ' instead' unless $src->isa('Ryu::Source');
+
+    $self = $self->new unless ref $self;
+    $src->each_while_source(sub {
+        $self->emit($_)
+    }, $self->source);
+    $src->completed->on_ready($self->source->completed);
+# $self->{source} = $src;
+    return $self
+}
+
+sub emit {
+    my ($self, $data) = @_;
+    $self->source->emit($data);
+    $self
+}
+
+sub source {
+    my ($self) = @_;
+    $self->{source} //= (
+        $self->{new_source} //= sub { Ryu::Source->new }
+    )->()
+}
+
+sub new_future {
+    my $self = shift;
+    (
+        $self->{new_future} //= sub {
+            Future->new->set_label(shift)
+        }
+    )->(@_)
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Tom Molesworth <TEAM@cpan.org>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2011-2019. Licensed under the same terms as Perl itself.
+
